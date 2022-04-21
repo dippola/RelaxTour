c7d5926a82050cc7df5654172be3c1a277409952
develiny <develiny9@gmail.com>
2022-04-21 20:54:06 +0900

    Initial commit13

--- app/src/main/java/com/dippola/relaxtour/BottomSheetAdapter.java
+++ app/src/main/java/com/dippola/relaxtour/BottomSheetAdapter.java
@@ -46,6 +46,12 @@ public class BottomSheetAdapter extends RecyclerView.Adapter<BottomSheetAdapter.
     @Override
     public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
         int positions = position;
+
+//        if (arrayList.size() != 0) {
+//            Bitmap bitmap = BitmapFactory.decodeByteArray(arrayList.get(position).getImg(), 0, arrayList.get(position).getImg().length);
+//            holder.button.setImageBitmap(bitmap);
+//        }
+
         Bitmap bitmap = BitmapFactory.decodeByteArray(arrayList.get(position).getImg(), 0, arrayList.get(position).getImg().length);
         holder.button.setImageBitmap(bitmap);
         holder.button.setOnClickListener(new View.OnClickListener() {
